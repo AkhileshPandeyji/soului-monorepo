8ada6b8134f8d7b721a09d7f5e2cc6e7bb539888
AkhileshPandeyji <pandeyakhilesh5372@gmail.com>
2024-09-24 00:11:28 +0530

    feat(setup): some necessary changes

--- packages/desktop/components/Button/Button.tsx
+++ packages/desktop/components/Button/Button.tsx
@@ -10,7 +10,7 @@ const Btn = styled.button<{
   width: ${({ width }) => width}px;
   height: ${({ height }) => height}px;
   background: ${({ bg }) => bg};
-  border-radius: 12px;
+  border-radius: 13px;
 `;
 
 interface ButtonProps {
